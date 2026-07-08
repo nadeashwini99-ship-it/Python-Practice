ea6862872f7a6aef405e4c6a38f2987ea04f89c6
Ashwini nade <nadeashwini99@gmail.com>
2026-07-08 13:10:42 +0530

    todo_list.py

---
+++ todo_list.py
@@ -0,0 +1,42 @@
+tasks = []
+
+while True:
+    print("\n===== TO-DO LIST =====")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task Added!")
+
+    elif choice == "2":
+        print("\nYour Tasks:")
+
+        if len(tasks) == 0:
+            print("No tasks available")
+
+        else:
+            for i in range(len(tasks)):
+                print(i + 1, ".", tasks[i])
+
+    elif choice == "3":
+        task = input("Enter task name to remove: ")
+
+        if task in tasks:
+            tasks.remove(task)
+            print("Task Removed!")
+
+        else:
+            print("Task not found")
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid Choice")
