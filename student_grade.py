6bc1928d28ba4bb04d0a52fa208431f34fd6d734
Ashwini nade <nadeashwini99@gmail.com>
2026-08-11 21:43:43 +0530

    student_grade.py

---
+++ student_grade.py
@@ -0,0 +1,20 @@
+name = input("Enter student name: ")
+marks = float(input("Enter marks: "))
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 80:
+    grade = "A"
+elif marks >= 70:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("\n----- Student Result -----")
+print("Name:", name)
+print("Marks:", marks)
+print("Grade:", grade)
